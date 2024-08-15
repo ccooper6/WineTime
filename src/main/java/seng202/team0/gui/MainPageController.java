4480c1e62337a23223100d47c1e536c019cc2efd
Caleb <cco139@uclive.ac.nz>
2024-08-15 16:03:00 +1200

    Tried a few fxml changes for the main page, not working yet.

---
+++ src/main/java/seng202/team0/gui/MainPageController.java
@@ -0,0 +1,10 @@
+package seng202.team0.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainPageController {
+    @FXML
+    Button wineButton;
+
+}
---
+++ src/main/java/seng202/team0/models/testWines/wine1.java
@@ -0,0 +1,10 @@
+package seng202.team0.models.testWines;
+
+import org.junit.Test;
+import seng202.team0.models.Wine;
+
+public class wine1 {
+    public wine1() {
+        Wine wine = new Wine("Red Wine", "Tasty", 50, "France", "French", "French1", "Grape", "Wine Time", null, null);
+    }
+}
