7cdc87749266285b0c09565e99acf0e960318d31
lydia <lja128@uclive.ac.nz>
2024-08-17 20:32:35 +1200

    changed the profile screen.

--- src/main/java/seng202/team0/gui/ProfileScreen.java
+++ src/main/java/seng202/team0/gui/ProfileScreen.java
@@ -1,4 +1,92 @@
 package seng202.team0.gui;
 
+import javafx.fxml.FXML;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import seng202.team0.models.User;
+
+import java.util.List;
+
 public class ProfileScreen {
+
+
+    @FXML
+    private Button changePasswordButton;
+    @FXML
+    private AnchorPane profilescrollpane;
+    @FXML
+    private  AnchorPane reveiwedWinePane;
+    @FXML
+    private  Label reviewedWineInfo;
+    @FXML
+    private  ImageView reviewedWineImage;
+    @FXML
+    private  AnchorPane wishlistWine1;
+    @FXML
+    private  Label wishlistWineInfo1;
+    @FXML
+    private  ImageView wishlistWineImage1;
+    @FXML
+    private  Label wishlistSeeMore;
+    @FXML
+    private  Label wishlistLabel;
+    @FXML
+    private  Label challengeLabel;
+    @FXML
+    private  ScrollPane challengePane;
+    @FXML
+    private  Label reviewsLabel;
+    @FXML
+    private  Label reviewsSeeMore;
+    @FXML
+    private  AnchorPane wishlistWine2;
+    @FXML
+    private  Label wishlistWineInfo2;
+    @FXML
+    private  ImageView wishlistWineImage2;
+    @FXML
+    private  AnchorPane wishlistWine3;
+    @FXML
+    private  Label wishlistWineInfo3;
+    @FXML
+    private  ImageView wishlistWineImage3;
+    @FXML
+    private  AnchorPane wishlistWine4;
+    @FXML
+    private  Label wishlistWineInfo4;
+    @FXML
+    private  ImageView wishlistWineImage4;
+    @FXML
+    private  FontAwesomeIconView wishlistSC;
+    @FXML
+    private  FontAwesomeIconView profileSC;
+    @FXML
+    private  FontAwesomeIconView ReviewsSC;
+    @FXML
+    private  ImageView logo;
+    @FXML
+    private  TextField searchBar;
+    @FXML
+    private  FontAwesomeIconView searchIcon;
+    @FXML
+    private  FontAwesomeIconView wishlistMoreArrow;
+
+
+    public void initialize() {
+        List<AnchorPane> wishlistWineView = List.of(wishlistWine1, wishlistWine2, wishlistWine3, wishlistWine4);
+    }
+
+
+    @FXML
+    public void onWishlistRefreshed() {
+//        wishlistMoreArrow.setOnMouseClicked(event -> );  need to pull favourite wines from user
+//        user.getFavouritedWines()
+    }
 }
+
+
