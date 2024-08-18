ad4a0219d4d58a30595d08ae843d8a5c21ab2e0b
Caleb <cco139@uclive.ac.nz>
2024-08-18 23:10:14 +1200

    Filled wine info popup using a test wine and formatted according to wireframes. Next step is to now start hooking up to the sql data from the csv file.

--- src/main/java/seng202/team0/gui/MainPageController.java
+++ src/main/java/seng202/team0/gui/MainPageController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -17,13 +19,13 @@ public class MainPageController {
     @FXML
     public void onWineButtonClicked() { // From advanced java fx tutorial
         try {
-            FXMLLoader addSaleLoader = new FXMLLoader(getClass().getResource("/fxml/popup.fxml"));
-            VBox root = addSaleLoader.load();
+            FXMLLoader winePopupLoader = new FXMLLoader(getClass().getResource("/fxml/popup.fxml"));
+            VBox root = winePopupLoader.load();
             Scene modalScene = new Scene(root);
             Stage modal = new Stage();
             modal.setScene(modalScene);
             modal.setWidth(500);
-            modal.setHeight(500);
+            modal.setHeight(400);
             modal.setResizable(false);
             modal.setTitle("Info");
             modal.initModality(Modality.WINDOW_MODAL);
@@ -31,5 +33,17 @@ public class MainPageController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    public void darkenButton(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        button.setStyle("-fx-background-color: #999999");
+    }
+
+    @FXML
+    public void lightenButton(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        button.setStyle("");
     }
 }
--- src/main/java/seng202/team0/models/testWines/wine1.java
+++ src/main/java/seng202/team0/models/testWines/wine1.java
@@ -1,10 +1,9 @@
 package seng202.team0.models.testWines;
 
-import org.junit.Test;
 import seng202.team0.models.Wine;
 
-public class wine1 {
+public class wine1 extends Wine {
     public wine1() {
-        Wine wine = new Wine("Red Wine", "Tasty", 50, "France", "French", "French1", "Grape", "Wine Time", null, null);
+        super("Red Wine", "Tasty", 50, "France", "French", "French1", "Grape", "Wine Time", null, null);
     }
 }
