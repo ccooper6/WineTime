bc0d446954f10550c03a22a5c2e3dc08c887111b
Isaac Macdonald <ima97@uclive.ac.nz>
2024-08-19 15:32:00 +1200

    Made it so the quiz clears after finishing the quiz.

--- src/main/java/seng202/team0/gui/QuizController.java
+++ src/main/java/seng202/team0/gui/QuizController.java
@@ -3,9 +3,12 @@ package seng202.team0.gui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import seng202.team0.services.QuizService;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
+
 
 public class QuizController {
 
@@ -20,75 +23,76 @@ public class QuizController {
     @FXML
     Button answer4Button;
 
-    ArrayList<String> questions = new ArrayList<>(Arrays.asList(
-            "You just robbed a chocolate store, what was your choice of poison?",
-            "It’s a Saturday night. What are you doing?",
-            "If you could only pick one fruit for the rest of your life what would it be?",
-            "What burger are you getting at McDonalds?",
-            "What Hogwarts house are you?"
-    ));
+    QuizService quizService = new QuizService();
 
-    ArrayList<String> answer1answers = new ArrayList<>(Arrays.asList(
-            "White chocolate",
-            "In bed with a book",
-            "Banana",
-            "Quarter pounder",
-            "Hufflepuff"
-    ));
-    ArrayList<String> answer2answers = new ArrayList<>(Arrays.asList(
-            "Milk chocolate",
-            "Asleep",
-            "Pear",
-            "McChicken",
-            "Ravenclaw"
-    ));
-    ArrayList<String> answer3answers = new ArrayList<>(Arrays.asList(
-            "Dark chocolate",
-            "Out on the town",
-            "Plum",
-            "Big Mac",
-            "Gryffindor"
-
-    ));
-    ArrayList<String> answer4answers = new ArrayList<>(Arrays.asList(
-            "The till",
-            "Casino",
-            "Peach",
-            "Filet o' Fish",
-            "Slytherin"
-    ));
+    private ArrayList<String> questions;
+    private ArrayList<String> answer1answers;
+    private ArrayList<String> answer2answers;
+    private ArrayList<String> answer3answers;
+    private ArrayList<String> answer4answers;
 
     int questionNumber = 0;
 
-    public void setLabels(){
-        questionLabel.setText(questions.get(questionNumber));
-        answer1Button.setText(answer1answers.get(questionNumber));
-        answer2Button.setText(answer2answers.get(questionNumber));
-        answer3Button.setText(answer3answers.get(questionNumber));
-        answer4Button.setText(answer4answers.get(questionNumber));
+    public void setLabels() {
+        if (questionNumber < questions.size()) {
+            questionLabel.setText(questions.get(questionNumber));
+            answer1Button.setText(answer1answers.get(questionNumber));
+            answer2Button.setText(answer2answers.get(questionNumber));
+            answer3Button.setText(answer3answers.get(questionNumber));
+            answer4Button.setText(answer4answers.get(questionNumber));
+        } else {
+            questionLabel.setOpacity(0);
+            answer1Button.setDisable(true);
+            answer2Button.setDisable(true);
+            answer3Button.setDisable(true);
+            answer4Button.setDisable(true);
+            answer1Button.setOpacity(0);
+            answer2Button.setOpacity(0);
+            answer3Button.setOpacity(0);
+            answer4Button.setOpacity(0);
+        }
     }
     @FXML
     public void initialize() {
+
+        questionLabel.setOpacity(1);
+        answer1Button.setDisable(false);
+        answer2Button.setDisable(false);
+        answer3Button.setDisable(false);
+        answer4Button.setDisable(false);
+        answer1Button.setOpacity(1);
+        answer2Button.setOpacity(1);
+        answer3Button.setOpacity(1);
+        answer4Button.setOpacity(1);
+        this.questions = quizService.getQuestions();
+        this.answer1answers = quizService.getAnswer1answers();
+        this.answer2answers = quizService.getAnswer2answers();
+        this.answer3answers = quizService.getAnswer3answers();
+        this.answer4answers = quizService.getAnswer4answers();
+
         setLabels();
     }
 
     @FXML
-    public void onAnswer1Clicked(){
+    public void onAnswer1Clicked() {
         questionNumber++;
         setLabels();
     }
+
     @FXML
-    public void onAnswer2Clicked(){
+    public void onAnswer2Clicked() {
         questionNumber++;
         setLabels();
     }
+
     @FXML
-    public void onAnswer3Clicked(){
+    public void onAnswer3Clicked() {
         questionNumber++;
         setLabels();
     }
+
     @FXML
-    public void onAnswer4Clicked(){
+    public void onAnswer4Clicked() {
         questionNumber++;
         setLabels();
     }
