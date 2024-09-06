cf92c359001b9ffd5c128c3f0e4f0f7038dc2256
lydia <lja128@uclive.ac.nz>
2024-09-06 16:15:26 +1200

    more progress on challenge service class

--- src/main/java/seng202/team0/services/ChallengeService.java
+++ src/main/java/seng202/team0/services/ChallengeService.java
@@ -1,13 +1,28 @@
 package seng202.team0.services;
 
+import com.sun.java.accessibility.util.SwingEventMonitor;
 import seng202.team0.repository.WineDAO;
 import seng202.team0.models.Wine;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class ChallengeService {
 
     WineDAO wineDAO = new WineDAO();
 
-//    Wine wine = wineDAO.getWhite();
+//    Set<String> whites = wineDAO.getWhite();
+//    this is the tags of white wines, need to get the wines from the db using these tags.
+    public void getTheWines() {
+//        List<Wine> challengeWines = new ArrayList<>;
+//        challengeWines.add(whites.get(0));
 
+//        for (String wine : whites) {
+//            System.out.println(wine);
+//        }
+    }
 
+    public static void main() {
+        (new ChallengeService()).getTheWines();
+    }
 }
