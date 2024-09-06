0693ea130ad8e49c3bd0b4ae36ad9be13bc4768b
elise-n-2431 <elise.n.2431@gmail.com>
2024-09-06 17:03:26 +1200

    Tried to merge main into this branch, mainpage.fxml is still to be merged.

--- src/main/java/seng202/team0/gui/MainController.java
+++ src/main/java/seng202/team0/gui/MainController.java
@@ -1,6 +1,9 @@
 package seng202.team0.gui;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -8,6 +11,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 import seng202.team0.models.Wine;
 import seng202.team0.models.testWines.*;
 
@@ -16,7 +20,7 @@ import java.util.List;
 
 /**
  * Controller class for the mainpage.fxml page
- * @author Caleb Cooper
+ * @author Caleb Cooper and Elise Newman
  */
 public class MainController {
     @FXML
@@ -30,6 +34,8 @@ public class MainController {
     @FXML
     AnchorPane mainWine4;
     @FXML
+    AnchorPane mainWine5;
+    @FXML
     Label wineInfo1;
     @FXML
     Label wineInfo2;
@@ -37,6 +43,8 @@ public class MainController {
     Label wineInfo3;
     @FXML
     Label wineInfo4;
+    @FXML
+    Label wineInfo5;
     @FXML
     ImageView mainWineIcon1;
     @FXML
@@ -46,7 +54,12 @@ public class MainController {
     @FXML
     ImageView mainWineIcon4;
     @FXML
+    ImageView mainWineIcon5;
+
+    @FXML
     FontAwesomeIconView scrollArrow;
+    int firstWine = 0;
+    List<AnchorPane> wineViews;
 
     // ***********TEST CASE WINE OBJECTS***************
     List<Wine> winesTest = new ArrayList<>(List.of(new wine1(), new wine2(), new wine3(), new wine4(), new wine5(), new wine6()));
@@ -66,29 +79,28 @@ public class MainController {
      * Initializes the controller.
      */
     public void initialize() {
-        helloText.setText("Hello, " + FXWrapper.getInstance().getCurrentUser().getName() + "!");
-        List<AnchorPane> wineViews = List.of(mainWine1, mainWine2, mainWine3, mainWine4);
-        List<Label> wineInfos = List.of(wineInfo1, wineInfo2, wineInfo3, wineInfo4);
-        List<ImageView> wineIcons = List.of(mainWineIcon1, mainWineIcon2, mainWineIcon3, mainWineIcon4);
-        displayWines(wineViews, wineInfos, wineIcons);
-        onRefresh(wineViews, wineInfos, wineIcons);
+        wineViews = List.of(mainWine1, mainWine2, mainWine3, mainWine4, mainWine5);
+        List<Label> wineInfos = List.of(wineInfo1, wineInfo2, wineInfo3, wineInfo4, wineInfo5);
+        List<ImageView> wineIcons = List.of(mainWineIcon1, mainWineIcon2, mainWineIcon3, mainWineIcon4, mainWineIcon5);
+        displayWines(wineInfos, wineIcons);
+        onRefresh(wineInfos, wineIcons);
     }
 
     /**
      * Sets each anchor pane to include the wine data of the corresponding wine object.
-     * @param wineView list of anchor panes
      * @param wineInfo list text boxes for the wine data
      * @param wineIcon list of wine icons corresponding to each wine
      */
     @FXML
-    public void displayWines(List<AnchorPane> wineView, List<Label> wineInfo, List<ImageView> wineIcon) {
-        if(winesTest.size() >= wineView.size()) {
-            for (int i = 0; i < wineView.size(); i++) {
-                wineInfo.get(i).setText(getWineInfo(winesTest.get(i)));
-                wineIcon.get(i).setImage(new Image(winesTest.get(i).getImagePath()));
+    public void displayWines(List<Label> wineInfo, List<ImageView> wineIcon) {
+        if(winesTest.size() >= wineViews.size()) {
+            for (int i = 0; i < wineViews.size(); i++) {
+                wineInfo.get(getId(i)).setText(String.valueOf(getId(i)));
+                //wineInfo.get(i).setText(getWineInfo(winesTest.get(i)));
+                wineIcon.get(getId(i)).setImage(new Image(winesTest.get(getId(i)).getImagePath()));
             }
         } else {
-            for (int i = winesTest.size(); i < wineView.size(); i++) {
+            for (int i = winesTest.size(); i < wineViews.size(); i++) {
                 wineInfo.get(i).setText("No wine available.");
                 wineIcon.get(i).setImage(null);
             }
@@ -97,18 +109,84 @@ public class MainController {
 
     /**
      * Refreshes the wine list in preparation to be displayed on the main page.
-     * @param wineView list of anchor panes
      * @param wineInfo list text boxes for the wine data
      * @param wineIcon list of wine icons corresponding to each wine
      */
     @FXML
-    public void onRefresh(List<AnchorPane> wineView, List<Label> wineInfo, List<ImageView> wineIcon) {
+    public void onRefresh( List<Label> wineInfo, List<ImageView> wineIcon) {
         scrollArrow.setOnMouseClicked(event -> {
-            Wine firstWine = winesTest.get(0);
-            winesTest.remove(0);
-            winesTest.add(firstWine);
-            displayWines(wineView, wineInfo, wineIcon);
+            transition();
+            displayWines(wineInfo, wineIcon);
         });
+    }
+
+    /**
+     * @param id is the position of the view pane.
+     * @return id of the wineView which should be displayed at that position.
+     */
+    public int getId(int id) {
+        return (firstWine + id) % wineViews.size();
+    }
+
+    /**
+     * The indexing of the wine views is reset
+     */
+    public void resetFirst() {
+        if (firstWine >= wineViews.size() - 1) {
+            firstWine = 0;
+        } else {
+            firstWine ++;
+        }
+        scrollArrow.setDisable(false);
+    }
+
+    /**
+     * As the first wine moves to the left, it decreases in opacity, is disabled and then teleports to the right;
+     */
+    public void fadeOut() {
+        FadeTransition fadeTransitionOut = new FadeTransition(Duration.seconds(0.5), wineViews.get(firstWine));
+        fadeTransitionOut.setFromValue(1);
+        fadeTransitionOut.setToValue(0);
+        fadeTransitionOut.play();
+        wineViews.get(firstWine).setDisable(true);
+        TranslateTransition transitionReturn = new TranslateTransition(Duration.seconds(0.1), wineViews.get(firstWine));
+        transitionReturn.setByX(1125);
+        transitionReturn.setInterpolator(Interpolator.DISCRETE);
+        fadeTransitionOut.setOnFinished(event -> transitionReturn.play());
+        transitionReturn.setOnFinished(event -> resetFirst());
+    }
+
+    /**
+     * As the last wine moves in from the right, it increases in opacity and is enabled.
+     */
+    public void fadeIn() {
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5), wineViews.get(getId(4)));
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(1);
+        fadeTransition.play();
+        fadeTransition.setOnFinished(event -> wineViews.get(getId(4)).setDisable(false));
+    }
+
+    /**
+     * Controls all the horizontal transitions of each category, triggered by onRefresh
+     */
+    @FXML
+    public void transition() {
+        scrollArrow.setDisable(true);
+        TranslateTransition transition1 = new TranslateTransition(Duration.seconds(0.5), wineViews.get(getId(0)));
+        TranslateTransition transition2 = new TranslateTransition(Duration.seconds(0.5), wineViews.get(getId(1)));
+        TranslateTransition transition3 = new TranslateTransition(Duration.seconds(0.5), wineViews.get(getId(2)));
+        TranslateTransition transition4 = new TranslateTransition(Duration.seconds(0.5), wineViews.get(getId(3)));
+        TranslateTransition transition5 = new TranslateTransition(Duration.seconds(0.5), wineViews.get(getId(4)));
+        List<TranslateTransition> wineTransitions = List.of(transition1, transition2, transition3, transition4, transition5);
+        for (int i = 0; i < wineTransitions.size(); i++) {
+            wineTransitions.get(i).setByX(-225);
+            wineTransitions.get(i).setInterpolator(Interpolator.LINEAR);
+            wineTransitions.get(i).play();
+        }
+        //below all occur at the same time as wineTransitions
+        fadeIn();
+        fadeOut();
     }
 
     /**
