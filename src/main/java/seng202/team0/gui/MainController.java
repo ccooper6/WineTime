8a687153fc26d4aad41f7e536d9b88a327f16713
Isaac Macdonald <ima97@uclive.ac.nz>
2024-09-08 11:30:26 +1200

    Added necessary changes in mainpage.fxml, MainController and WineCategoryDisplayController as well as a new service class to display 8 catergorys on the main page.

--- src/main/java/seng202/team0/gui/MainController.java
+++ src/main/java/seng202/team0/gui/MainController.java
@@ -12,9 +12,11 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import seng202.team0.models.Wine;
 import seng202.team0.services.SearchWineService;
+import seng202.team0.services.WineCategoryService;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -29,16 +31,49 @@ public class MainController {
      * Initializes the controller.
      */
     public void initialize() {
-        SearchWineService.getInstance().searchWinesByName("Rainstorm");
+        SearchWineService.getInstance().searchWinesByTags("Bordeaux, Merlot");
 
         try {
             FXMLLoader fxmlLoader1 = new FXMLLoader(getClass().getResource("/fxml/wineCategoryDisplay.fxml"));
             Parent parent1 = fxmlLoader1.load();
             contentsGrid.add(parent1, 0, 0);
-//            SearchWineService.getInstance().searchWinesByName("New Zealand, Sauvignon Blanc");
-//            FXMLLoader fxmlLoader2 = new FXMLLoader(getClass().getResource("/fxml/wineCategoryDisplay.fxml"));
-//            Parent parent2 = fxmlLoader2.load();
-//            contentsGrid.add(parent2, 1, 1);
+            WineCategoryService.getInstance().incrementCurrentCategory();
+            SearchWineService.getInstance().searchWinesByTags("Marlborough, Sauvignon Blanc");
+            FXMLLoader fxmlLoader2 = new FXMLLoader(getClass().getResource("/fxml/wineCategoryDisplay.fxml"));
+            Parent parent2 = fxmlLoader2.load();
+            contentsGrid.add(parent2, 0, 1);
+            WineCategoryService.getInstance().incrementCurrentCategory();
+            SearchWineService.getInstance().searchWinesByTags("Tuscany, Sangiovese");
+            FXMLLoader fxmlLoader3 = new FXMLLoader(getClass().getResource("/fxml/wineCategoryDisplay.fxml"));
+            Parent parent3 = fxmlLoader3.load();
+            contentsGrid.add(parent3, 0, 2);
+            WineCategoryService.getInstance().incrementCurrentCategory();
+            SearchWineService.getInstance().searchWinesByTags("Hawke's Bay, Syrah");
+            FXMLLoader fxmlLoader4 = new FXMLLoader(getClass().getResource("/fxml/wineCategoryDisplay.fxml"));
+            Parent parent4 = fxmlLoader4.load();
+            contentsGrid.add(parent4, 0, 3);
+            WineCategoryService.getInstance().incrementCurrentCategory();
+            SearchWineService.getInstance().searchWinesByTags("Spain, Rioja, Tempranillo");
+            FXMLLoader fxmlLoader5 = new FXMLLoader(getClass().getResource("/fxml/wineCategoryDisplay.fxml"));
+            Parent parent5 = fxmlLoader5.load();
+            contentsGrid.add(parent5, 0, 4);
+            WineCategoryService.getInstance().incrementCurrentCategory();
+            SearchWineService.getInstance().searchWinesByTags("New Zealand, Gisborne, Chardonnay");
+            FXMLLoader fxmlLoader6 = new FXMLLoader(getClass().getResource("/fxml/wineCategoryDisplay.fxml"));
+            Parent parent6 = fxmlLoader6.load();
+            contentsGrid.add(parent6, 0, 5);
+            WineCategoryService.getInstance().incrementCurrentCategory();
+            SearchWineService.getInstance().searchWinesByTags("US, Napa Valley, Cabernet Sauvignon");
+            FXMLLoader fxmlLoader7 = new FXMLLoader(getClass().getResource("/fxml/wineCategoryDisplay.fxml"));
+            Parent parent7 = fxmlLoader7.load();
+            contentsGrid.add(parent7, 0, 6);
+            WineCategoryService.getInstance().incrementCurrentCategory();
+            SearchWineService.getInstance().searchWinesByTags("Central Otago, Pinot Noir, New Zealand");
+            FXMLLoader fxmlLoader8 = new FXMLLoader(getClass().getResource("/fxml/wineCategoryDisplay.fxml"));
+            Parent parent8 = fxmlLoader8.load();
+            contentsGrid.add(parent8, 0, 7);
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -81,4 +116,6 @@ public class MainController {
         AnchorPane pane = (AnchorPane) event.getSource();
         pane.setStyle("-fx-border-color: #d9d9d9; -fx-border-radius: 15");
     }
+
+
 }
--- src/main/java/seng202/team0/gui/WineCategoryDisplayController.java
+++ src/main/java/seng202/team0/gui/WineCategoryDisplayController.java
@@ -1,5 +1,6 @@
 package seng202.team0.gui;
 
+import com.sun.tools.javac.Main;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,13 +9,15 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import seng202.team0.models.Wine;
 import seng202.team0.services.SearchWineService;
+import seng202.team0.services.WineCategoryService;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WineCategoryDisplayController {
     @FXML
-    Text titeText;
+    Text titleText;
 
     @FXML
     GridPane wineGrid;
@@ -33,7 +36,7 @@ public class WineCategoryDisplayController {
     {
         ArrayList<Wine> displayWines = SearchWineService.getInstance().getWineList();
         wineDisplays = new ArrayList<>();
-
+        titleText.setText(WineCategoryService.getInstance().getCategoryTitles().get(WineCategoryService.getInstance().getCurrentCategory()));
         for (int i = 0; i < Math.min(displayWines.size(), MAXWINES); i++) {
             SearchWineService.getInstance().setCurrentWine(displayWines.get(i));
 
@@ -46,6 +49,12 @@ public class WineCategoryDisplayController {
         }
 
         displayWines();
+    }
+
+    @FXML
+    public void setTitleText(String title)
+    {
+        titleText.setText(title);
     }
 
     @FXML
