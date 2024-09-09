72ae420465d1fae155869c1bc03ed35e58f8dfeb
lydia <lja128@uclive.ac.nz>
2024-09-09 16:00:35 +1200

    changed how the wine display on the profile

--- src/main/java/seng202/team0/gui/ProfileController.java
+++ src/main/java/seng202/team0/gui/ProfileController.java
@@ -3,6 +3,8 @@ package seng202.team0.gui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -11,9 +13,13 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import org.apache.commons.collections.functors.FalsePredicate;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import seng202.team0.models.Wine;
 import seng202.team0.services.SearchWineService;
+import seng202.team0.services.WineCategoryService;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -100,6 +106,10 @@ public class ProfileController {
     @FXML
     private Label challengeIntro;
 
+    @FXML
+    private AnchorPane wishlistPane;
+
+    private static final Logger log = LogManager.getLogger(ProfileController.class);
 
     List<Wine> wineList;
     // consider adding a wine info aspect to wine class so u can get the string description from wines
@@ -112,10 +122,10 @@ public class ProfileController {
 
 
     public void initialize() {
-        List<AnchorPane> wishlistWineView = List.of(wishlistWine1, wishlistWine2, wishlistWine3, wishlistWine4);
-        List<Label> wishlistWineInfo = List.of(wishlistWineInfo1, wishlistWineInfo2, wishlistWineInfo3, wishlistWineInfo4);
-        displayWishlist(wishlistWineView, wishlistWineInfo);
-        onWishlistRefreshed(wishlistWineView,  wishlistWineInfo);
+//        List<AnchorPane> wishlistWineView = List.of(wishlistWine1, wishlistWine2, wishlistWine3, wishlistWine4);
+//        List<Label> wishlistWineInfo = List.of(wishlistWineInfo1, wishlistWineInfo2, wishlistWineInfo3, wishlistWineInfo4);
+//        displayWishlist(wishlistWineView, wishlistWineInfo);
+//        onWishlistRefreshed(wishlistWineView,  wishlistWineInfo);
         challengePane.setVisible(false);
 //        FXWrapper.getInstance().setChallenge(1);
         System.out.println(FXWrapper.getInstance().getChallenge());
@@ -126,23 +136,23 @@ public class ProfileController {
 
         }
 
+        displayWishlist();
+
     }
 
 //    on wineView clicked, open wine pop up, pass the relevant information from the wine. make a method to do this,
 //    this functionality will be needed for mulitple screens.
 
     @FXML
-    public void displayWishlist(List<AnchorPane> wishlistWineView, List<Label> wishlistWineInfo) {
-        SearchWineService.getInstance().searchWinesByName("Stemmari");
-        wineList = SearchWineService.getInstance().getWineList();
-        if(wineList.size() >= wishlistWineView.size()) {
-            for (int i = 0; i < wishlistWineView.size(); i++) {
-                wishlistWineInfo.get(i).setText(getWineInfo(wineList.get(i)));
-            }
-        } else {
-            for (int i = wineList.size(); i < wishlistWineView.size(); i++) {
-                wishlistWineInfo.get(i).setText("No wine available.");
-            }
+    public void displayWishlist() {
+        WineCategoryService.getInstance().resetCurrentCategory();
+        SearchWineService.getInstance().searchWinesByTags("Bordeaux, Merlot");
+        try {
+            FXMLLoader fxmlLoader1 = new FXMLLoader(getClass().getResource("/fxml/wineCategoryDisplay.fxml"));
+            Parent parent1 = fxmlLoader1.load();
+            wishlistPane.getChildren().add(parent1);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -153,7 +163,7 @@ public class ProfileController {
             Wine firtWine = wineList.getFirst();
             wineList.removeFirst();
             wineList.add(firtWine);
-            displayWishlist(wishlistWineView, wishlistWineInfo);
+//            displayWishlist(wishlistWineView, wishlistWineInfo);
         });
 //
     }
