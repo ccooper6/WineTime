075581045763c78ad5f75acea1bccdbcc1d9910e
lja128 <lja128@uclive.ac.nz>
2024-09-14 12:18:40 +1200

    fixed something so you can get in and out of the search page.

--- src/main/java/seng202/team1/gui/NavigationController.java
+++ src/main/java/seng202/team1/gui/NavigationController.java
@@ -107,7 +107,8 @@ public class NavigationController {
                     SearchWineService.getInstance().searchWinesByTags(searchBar.getText());
                 }
 
-                FXWrapper.getInstance().launchSubPage("searchWine");
+//                FXWrapper.getInstance().launchSubPage("searchWine");
+                loadPageContent("searchWine");
 //                searchBar.clear();
                 searchBar.getParent().requestFocus();
             }
