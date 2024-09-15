28657b9b77b35792220076c394bf41a5a95bf928
Wen Sheng Thong <wst44@uclive.ac.nz>
2024-09-15 15:46:55 +1200

    the unit tests for LogWineDao

---
+++ src/test/java/seng202/team1/unittests/services/LogWineDAOTest.java
@@ -0,0 +1,252 @@
+package seng202.team1.unittests.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import seng202.team1.exceptions.InstanceAlreadyExistsException;
+import seng202.team1.models.Review;
+import seng202.team1.repository.DatabaseManager;
+import seng202.team1.repository.LogWineDao;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * The unit test for the {@link LogWineDao} class
+ *
+ * @author Wen Sheng Thong
+ */
+public class LogWineDAOTest {
+    static DatabaseManager databaseManager;
+    static LogWineDao logWineDao;
+    /**
+     * Makes sure the database is set up before each test. Overwrites the prev test database with a clean test_database
+     * before each test.
+     * @throws InstanceAlreadyExistsException
+     */
+    @BeforeAll
+    static void setUp() throws InstanceAlreadyExistsException {
+        DatabaseManager.REMOVE_INSTANCE();
+        databaseManager = DatabaseManager.initialiseInstanceWithUrl("jdbc:sqlite:./src/test/resources/test_database.db");
+        logWineDao = new LogWineDao();
+
+        String copyPath = databaseManager.url.substring(12);
+        Path copy = Paths.get(copyPath);
+        Path ogPath = Paths.get("src/main/resources/sql/og.db");
+        try {
+            Files.copy(ogPath, copy, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Checks to see if likes are being added to the database accurately, also test the getLikes method
+     */
+    @Test
+    public void testAddingLikes() {
+        logWineDao.likes(1, "tag1", 5);
+        HashMap<String, Integer> result = logWineDao.getLikedTags(1, 10, true);
+        Assertions.assertTrue(result.containsKey("tag1"));
+        Assertions.assertEquals(5,result.get("tag1"));
+    }
+
+    /**
+     * Test to ensure that tag values are different for different users
+     */
+    @Test
+    public void testAddingDiffUserLikes() {
+        logWineDao.likes(1, "tag1", 5);
+        logWineDao.likes(2, "tag1", -4);
+        HashMap<String, Integer> result = logWineDao.getLikedTags(1, 10, true);
+        Assertions.assertTrue(result.containsKey("tag1"));
+        Assertions.assertEquals(5,result.get("tag1"));
+        HashMap<String, Integer> result2 = logWineDao.getLikedTags(2, 10, true);
+        Assertions.assertTrue(result2.containsKey("tag1"));
+        Assertions.assertEquals(-4,result2.get("tag1"));
+    }
+
+    /**
+     * Checks that {@link LogWineDao#updateLikesValue(int, String, int)} successfully updates the likes value
+     */
+    @Test
+    public void testUpdateLikes() {
+        logWineDao.likes(1, "tag1", 10);
+        logWineDao.updateLikesValue(1, "tag1", -10);
+        HashMap<String, Integer> result = logWineDao.getLikedTags(1, 10, true);
+        Assertions.assertTrue(result.containsKey("tag1"));
+        Assertions.assertEquals(0,result.get("tag1"));
+    }
+
+    /**
+     * Checks that {@link LogWineDao#likes(int, String, int)} calls {@link LogWineDao#updateLikesValue(int, String, int)}
+     * properly
+     */
+    @Test
+    public void testLikesCallsUpdateLikes() {
+        logWineDao.likes(1, "tag1", 10);
+        logWineDao.likes(1, "tag1", 10);
+        HashMap<String, Integer> result = logWineDao.getLikedTags(1, 10, true);
+        Assertions.assertTrue(result.containsKey("tag1"));
+        Assertions.assertEquals(20,result.get("tag1"));
+    }
+
+    /**
+     * Checks that {@link LogWineDao#getLikedTags(int, int, boolean)} returns the top valued tags and that the maximum
+     * tag returned is working
+     */
+    @Test
+    public void testGetLikedTags() {
+        logWineDao.likes(1, "tag1", 10);
+        logWineDao.likes(1, "tag2", 20);
+        logWineDao.likes(1, "tag3", -100);
+        HashMap<String, Integer> result = logWineDao.getLikedTags(1, 2, true);
+        Assertions.assertTrue(result.containsKey("tag1"));
+        Assertions.assertEquals(10,result.get("tag1"));
+        Assertions.assertTrue(result.containsKey("tag2"));
+        Assertions.assertEquals(20,result.get("tag2"));
+        Assertions.assertFalse(result.containsKey("tag3"));
+    }
+
+    /**
+     * Checks that {@link LogWineDao#getLikedTags(int, int, boolean)} does not break when user has no liked tags
+     * and properly returns an empty hashmap.
+     */
+    @Test
+    public void testNoTags() {
+        HashMap<String, Integer> result = logWineDao.getLikedTags(1, 2, true);
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    /**
+     * Checks that {@link LogWineDao#getLikedTags(int, boolean)} properly returns all the user's liked tags.
+     */
+    @Test
+    public void testGetLikedNoLimit() {
+        logWineDao.likes(1, "tag1", 10);
+        logWineDao.likes(1, "tag2", 20);
+        logWineDao.likes(1, "tag3", -100);
+        HashMap<String, Integer> result = logWineDao.getLikedTags(1, true);
+        Assertions.assertTrue(result.size() == 3);
+        Assertions.assertTrue(result.containsKey("tag1"));
+        Assertions.assertEquals(10,result.get("tag1"));
+        Assertions.assertTrue(result.containsKey("tag2"));
+        Assertions.assertEquals(20,result.get("tag2"));
+        Assertions.assertTrue(result.containsKey("tag3"));
+        Assertions.assertEquals(-100,result.get("tag3"));
+    }
+
+    /**
+     * Checks that {@link LogWineDao#reviews(int, int, int, String, String)} properly adds a review to the database as
+     * well as tests that {@link LogWineDao#getUserReview(int, Boolean)} properly returns the user's reviews
+     */
+    @Test
+    public void testAddingReview() {
+        logWineDao.reviews(1,2,3,"I like wine", "2024-09-15 14:59:51");
+        ArrayList<Review> reviews = logWineDao.getUserReview(1, false);
+        Assertions.assertEquals(1,reviews.getFirst().getUid());
+        Assertions.assertEquals(2,reviews.getFirst().getWid());
+        Assertions.assertEquals(3,reviews.getFirst().getRating());
+        Assertions.assertEquals("I like wine",reviews.getFirst().getReviewDescription());
+        Assertions.assertEquals("2024-09-15 14:59:51",reviews.getFirst().getReviewDate());
+    }
+    /**
+     * Checks that {@link LogWineDao#reviews(int, int, int, String, String)} properly adds a review to the database as
+     * well as tests that {@link LogWineDao#getUserReview(int, Boolean)} properly returns the user's reviews correctly
+     * based on the user id.
+     */
+    @Test
+    public void testAddingGettingDiffReview() {
+        logWineDao.reviews(1,2,3,"I like wine", "2024-09-15 14:59:51");
+        logWineDao.reviews(2,6,3,"I really like wine", "2025-09-15 14:59:51");
+        ArrayList<Review> reviews = logWineDao.getUserReview(1, false);
+        ArrayList<Review> reviews2 = logWineDao.getUserReview(2, false);
+        Assertions.assertEquals(1,reviews.getFirst().getUid());
+        Assertions.assertEquals(2,reviews.getFirst().getWid());
+        Assertions.assertEquals(3,reviews.getFirst().getRating());
+        Assertions.assertEquals("I like wine",reviews.getFirst().getReviewDescription());
+        Assertions.assertEquals("2024-09-15 14:59:51",reviews.getFirst().getReviewDate());
+        Assertions.assertEquals(2,reviews2.getFirst().getUid());
+        Assertions.assertEquals(6,reviews2.getFirst().getWid());
+        Assertions.assertEquals(3,reviews2.getFirst().getRating());
+        Assertions.assertEquals("I really like wine",reviews2.getFirst().getReviewDescription());
+        Assertions.assertEquals("2025-09-15 14:59:51",reviews2.getFirst().getReviewDate());
+    }
+
+    /**
+     * Checks that {@link LogWineDao#getUserReview(int, int, Boolean)} returns up to only the specified number of reviews
+     * and sorts it by most recent.
+     */
+    @Test
+    public void testGettingOneRecentReview() {
+        logWineDao.reviews(1,3,-1,"I hate wine", "2023-09-15 14:59:51");
+        logWineDao.reviews(1,5,-1,"I hate 5 wine", "2022-09-15 14:59:51");
+        logWineDao.reviews(1,6,-1,"I hate 6 wine", "2022-09-15 14:59:51");
+        logWineDao.reviews(1,2,3,"I like wine", "2024-09-15 14:59:51");
+        ArrayList<Review> reviews = logWineDao.getUserReview(1,1, true);
+        Assertions.assertEquals(1,reviews.getFirst().getUid());
+        Assertions.assertEquals(2,reviews.getFirst().getWid());
+        Assertions.assertEquals(3,reviews.getFirst().getRating());
+        Assertions.assertEquals("I like wine",reviews.getFirst().getReviewDescription());
+        Assertions.assertEquals("2024-09-15 14:59:51",reviews.getFirst().getReviewDate());
+        Assertions.assertEquals(1, reviews.size());
+    }
+
+    /**
+     * Checks that {@link LogWineDao#reviews(int, int, int, String, String) calls
+     * {@link LogWineDao#updateReview(int, int, int, String, String)}} correctly and updates the review appropriately
+     * and that {@link LogWineDao#alreadyReviewExists(int, int)} returns the right boolean.
+     */
+    @Test
+    public void testUpdatingReview() {
+        logWineDao.reviews(1,2,3,"I like wine", "2024-09-15 14:59:51");
+        logWineDao.reviews(1,2,-10,"I no longer like wine", "2024-10-15 14:59:51");
+        ArrayList<Review> reviews = logWineDao.getUserReview(1, false);
+        Assertions.assertEquals(1,reviews.getFirst().getUid());
+        Assertions.assertEquals(2,reviews.getFirst().getWid());
+        Assertions.assertEquals(-10,reviews.getFirst().getRating());
+        Assertions.assertEquals("I no longer like wine",reviews.getFirst().getReviewDescription());
+        Assertions.assertEquals("2024-10-15 14:59:51",reviews.getFirst().getReviewDate());
+        Assertions.assertEquals(1, reviews.size());
+    }
+
+    /**
+     * Checks that {@link LogWineDao#getUserReview(int, int, Boolean)} returns a properly sorted array list of Reviews
+     */
+    @Test
+    public void testOrderedReviews() {
+        logWineDao.reviews(1,4,10,"I can travel to future", "2026-10-15 14:59:51");
+        logWineDao.reviews(1,2,3,"I like wine", "2024-09-15 14:59:51");
+        logWineDao.reviews(1,3,-10,"I no longer like wine", "2025-10-15 14:59:51");
+        ArrayList<Review> reviews = logWineDao.getUserReview(1, true);
+        Assertions.assertEquals(1,reviews.getFirst().getUid());
+        Assertions.assertEquals(4,reviews.getFirst().getWid());
+        Assertions.assertEquals(10,reviews.getFirst().getRating());
+        Assertions.assertEquals("I can travel to future",reviews.getFirst().getReviewDescription());
+        Assertions.assertEquals("2026-10-15 14:59:51",reviews.getFirst().getReviewDate());
+        Assertions.assertEquals(1,reviews.get(1).getUid());
+        Assertions.assertEquals(3,reviews.get(1).getWid());
+        Assertions.assertEquals(-10,reviews.get(1).getRating());
+        Assertions.assertEquals("I no longer like wine",reviews.get(1).getReviewDescription());
+        Assertions.assertEquals("2025-10-15 14:59:51",reviews.get(1).getReviewDate());
+        Assertions.assertEquals(1,reviews.get(2).getUid());
+        Assertions.assertEquals(2,reviews.get(2).getWid());
+        Assertions.assertEquals(3,reviews.get(2).getRating());
+        Assertions.assertEquals("I like wine",reviews.get(2).getReviewDescription());
+        Assertions.assertEquals("2024-09-15 14:59:51",reviews.get(2).getReviewDate());
+    }
+
+    /**
+     * Checks that an empty ArrayList is returned when no reviews are found.
+     */
+    @Test
+    public void testNoReviews() {
+        ArrayList<Review> reviews = logWineDao.getUserReview(1, true);
+        Assertions.assertTrue(reviews.isEmpty());
+    }
+}
