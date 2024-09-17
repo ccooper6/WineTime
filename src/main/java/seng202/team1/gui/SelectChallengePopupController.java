c82377ea54e6d6e50651372e3f5b6f454890ee47
lja128 <lja128@uclive.ac.nz>
2024-09-17 13:14:45 +1200

    refactored code so that the controllers don't communicate directly with dao but go through the service class.

--- src/main/java/seng202/team1/gui/SelectChallengePopupController.java
+++ src/main/java/seng202/team1/gui/SelectChallengePopupController.java
@@ -5,6 +5,7 @@ import javafx.scene.input.MouseEvent;
 import seng202.team1.models.User;
 import seng202.team1.repository.ChallengeDAO;
 import seng202.team1.repository.DatabaseManager;
+import seng202.team1.services.ChallengeService;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -12,10 +13,7 @@ import java.sql.SQLException;
 
 public class SelectChallengePopupController {
 
-    private int currentUserID;
-
-    private DatabaseManager databaseManager;
-    private ChallengeDAO chalDao;
+    ChallengeService challengeService = new ChallengeService();
 
 
     /**
@@ -24,21 +22,8 @@ public class SelectChallengePopupController {
      */
 
     public void startVarietyChallenge(ActionEvent actionEvent) {
-//        close popup
-//        launch profile
-//move wine pane dow
-        databaseManager = DatabaseManager.getInstance();
-//        currentUserID = getUId(FXWrapper.getInstance().getCurrentUser());
-        chalDao = new ChallengeDAO();
-        currentUserID = chalDao.getUId(FXWrapper.getInstance().getCurrentUser());
-        chalDao.userActivatesChallenge(currentUserID, "Variety Challenge");
-        chalDao.wineInChallenge();
-        System.out.println("started challenge");
-//        FXWrapper.getInstance().setChallenge(1);
-        NavigationController navigationController = FXWrapper.getInstance().getNavigationController();
-        navigationController.closePopUp();
-        navigationController.loadPageContent("profile");
-
+        challengeService.startChallengeVariety();
+        challengeService.launchProfile();
     }
 
 
@@ -47,10 +32,7 @@ public class SelectChallengePopupController {
      * @param mouseEvent
      */
     public void closeChallengePopup(MouseEvent mouseEvent) {
-//        close popup
-//        launch profile
-        NavigationController navigationController = FXWrapper.getInstance().getNavigationController();
-        navigationController.closePopUp();
+        challengeService.closeSelectChallenge();
 
     }
 
--- src/main/java/seng202/team1/services/ChallengeService.java
+++ src/main/java/seng202/team1/services/ChallengeService.java
@@ -1,12 +1,84 @@
 package seng202.team1.services;
 
+import seng202.team1.gui.FXWrapper;
+import seng202.team1.gui.NavigationController;
 import seng202.team1.models.Wine;
-import seng202.team1.repository.SearchDAO;
+import seng202.team1.repository.ChallengeDAO;
+import seng202.team1.repository.DatabaseManager;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
+
+/**
+ * Service class for the challenge tracker feature.
+ * @author Lydia Jackson
+ */
 
 public class ChallengeService {
+    private DatabaseManager databaseManager = DatabaseManager.getInstance();
+    private ChallengeDAO chalDao = new ChallengeDAO();
+    private int currentUserID = chalDao.getUId(FXWrapper.getInstance().getCurrentUser());
+
+
+    /**
+     * calls the cahllengeDAO to update the database so that the user has the variety challenge as an active challenge.
+     */
+    public void startChallengeVariety() {
+        chalDao.userActivatesChallenge(currentUserID, "Variety Challenge");
+        chalDao.wineInChallenge();
+
+    }
+
+    /**
+     * launches/resets the profile.
+     */
+    public void launchProfile() {
+        NavigationController navigationController = FXWrapper.getInstance().getNavigationController();
+        navigationController.closePopUp();
+        navigationController.loadPageContent("profile");
+    }
+
+    /**
+     * launches the select challenge pop up.
+     */
+    public void launchSelectChallenge() {
+        NavigationController navigationController = FXWrapper.getInstance().getNavigationController();
+        navigationController.closePopUp();
+        navigationController.loadSelectChallengePopUpContent();
+    }
+
+    /**
+     * closes the select challenge pop up.
+     */
+    public void closeSelectChallenge() {
+        NavigationController navigationController = FXWrapper.getInstance().getNavigationController();
+        navigationController.closePopUp();
+    }
+
+
+    /**
+     * checks to see if user has the variety challenge as an active challenge.
+     * @return true if the user has the variety challenge.
+     */
+    public boolean activeChallenge() {
+        return Objects.equals(chalDao.getChallengeForUser(currentUserID), "Variety Challenge");
+    }
+
+    /**
+     * gets the wines for the challenge that the user has active.
+     * @return arraylist of the wines for the challenge the user is participating in.
+     */
+    public ArrayList<Wine> challengeWines() {
+        return chalDao.getWinesForChallenge(chalDao.getChallengeForUser(currentUserID));
+    }
+
+    /**
+     * gets the name of the challenge the user has active.
+     * @return string name of users active challenge.
+     */
+    public String usersChallenge() {
+        return chalDao.getChallengeForUser(currentUserID);
+    }
 
 }
 
