fa1605d71399da53e50f3a3c607247087c363a98
Wen Sheng Thong <wst44@uclive.ac.nz>
2024-10-08 12:35:44 +1300

    added gifs and images for help screen as well as centered it, also changed the search in nav controller to search 'helpScreen' instead of 'help'

--- src/main/java/seng202/team1/gui/controllers/HelpController.java
+++ src/main/java/seng202/team1/gui/controllers/HelpController.java
@@ -1,6 +1,7 @@
 package seng202.team1.gui.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class HelpController {
@@ -18,15 +19,27 @@ public class HelpController {
     private ImageView reviewWineHelp;
 
     @FXML
-    private ImageView reviewWineHelp1;
+    private ImageView wishListHelp;
 
     @FXML
     private ImageView searchPageHelp;
 
     @FXML
     private ImageView winePopUpHelp;
+    @FXML
+    private ImageView wineTimeText;
+    @FXML
+    private ImageView wineTimeLogo;
 
     public void initialize() {
+        homePageHelp.setImage(new Image(getClass().getResourceAsStream("/images/helpScreen/homePageHelp.gif")));
+        loginHelp.setImage(new Image(getClass().getResourceAsStream("/images/helpScreen/loginHelp.gif")));
+        profilePageHelp.setImage(new Image(getClass().getResourceAsStream("/images/helpScreen/profileHelp.gif")));
+        reviewWineHelp.setImage(new Image(getClass().getResourceAsStream("/images/helpScreen/reviewWineHelp.gif")));
+        wishListHelp.setImage(new Image(getClass().getResourceAsStream("/images/helpScreen/wishListHelp.gif")));
+        winePopUpHelp.setImage(new Image(getClass().getResourceAsStream("/images/helpScreen/winePopupHelp.gif")));
+        searchPageHelp.setImage(new Image(getClass().getResourceAsStream("/images/helpScreen/searchHelp.gif")));
+        wineTimeLogo.setImage(new Image(getClass().getResourceAsStream("/images/logo.png")));
+        wineTimeText.setImage(new Image(getClass().getResourceAsStream("/images/winetime-nobg.png")));
     }
-
 }
