43a927a5d11b79195839ff52c48d49be624ffd1b
Wen Sheng Thong <wst44@uclive.ac.nz>
2024-10-08 10:54:19 +1300

    added helpcontroller to help screen

---
+++ src/main/java/seng202/team1/gui/controllers/HelpController.java
@@ -0,0 +1,32 @@
+package seng202.team1.gui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class HelpController {
+
+    @FXML
+    private ImageView homePageHelp;
+
+    @FXML
+    private ImageView loginHelp;
+
+    @FXML
+    private ImageView profilePageHelp;
+
+    @FXML
+    private ImageView reviewWineHelp;
+
+    @FXML
+    private ImageView reviewWineHelp1;
+
+    @FXML
+    private ImageView searchPageHelp;
+
+    @FXML
+    private ImageView winePopUpHelp;
+
+    public void initialize() {
+    }
+
+}
