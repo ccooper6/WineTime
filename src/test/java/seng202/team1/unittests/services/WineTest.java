810129f21628184ec38ba7959f535c38173e04e4
Caleb <cco139@uclive.ac.nz>
2024-10-09 18:31:41 +1300

    Added tests to wine test

--- src/test/java/seng202/team1/unittests/services/WineTest.java
+++ src/test/java/seng202/team1/unittests/services/WineTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import seng202.team1.models.TagType;
 import seng202.team1.models.Wine;
 import seng202.team1.models.WineBuilder;
+import seng202.team1.services.WineVarietyService;
+
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,8 +22,27 @@ public class WineTest {
     }
 
     @Test
-    void testGetImagePath() {
+    void testGetImagePathRed() {
+        wine.setVariety("Cabernet Sauvignon");
         assertEquals(wine.getImagePath(), getClass().getResource("/images/Red Wine.jpg").toExternalForm());
+    }
+
+    @Test
+    void testGetImagePathWhite() {
+        wine.setVariety("Chardonnay");
+        assertEquals(wine.getImagePath(), getClass().getResource("/images/White Wine.jpg").toExternalForm());
+    }
+
+    @Test
+    void testGetImagePathRose() {
+        wine.setVariety("Rosé");
+        assertEquals(wine.getImagePath(), getClass().getResource("/images/Rose Wine.jpg").toExternalForm());
+    }
+
+    @Test
+    void testGetImagePathSparkling() {
+        wine.setVariety("Champagne Blend");
+        assertEquals(wine.getImagePath(), getClass().getResource("/images/Sparkling Wine.jpg").toExternalForm());
     }
 
     @Test
@@ -52,7 +74,43 @@ public class WineTest {
     }
 
     @Test
-    void hasTag() {
+    void hasVintageTag() {
+        wine.setVintage(2020);
+        assertTrue(wine.hasTag("2020"));
+    }
+
+    @Test
+    void hasCountryTag() {
+        wine.setCountry("Test");
+        assertTrue(wine.hasTag("Test"));
+    }
+
+    @Test
+    void hasProvinceTag() {
+        wine.setProvince("Test");
+        assertTrue(wine.hasTag("Test"));
+    }
+
+    @Test
+    void hasRegion1Tag() {
+        wine.setRegion1("Test");
+        assertTrue(wine.hasTag("Test"));
+    }
+
+    @Test
+    void hasRegion2Tag() {
+        wine.setRegion2("Test");
+        assertTrue(wine.hasTag("Test"));
+    }
+
+    @Test
+    void hasVarietyTag() {
+        wine.setVariety("Test");
+        assertTrue(wine.hasTag("Test"));
+    }
+
+    @Test
+    void hasWineryTag() {
         wine.setWinery("Test");
         assertTrue(wine.hasTag("Test"));
     }
