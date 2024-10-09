a50117250f85d66daced8d895ef514863fa941a0
Wen Sheng Thong <wst44@uclive.ac.nz>
2024-10-09 21:08:21 +1300

    added missing doc strings

--- src/main/java/seng202/team1/repository/DAOs/TagDAO.java
+++ src/main/java/seng202/team1/repository/DAOs/TagDAO.java
@@ -126,6 +126,12 @@ public class TagDAO {
         return getIntTag(sql, TagType.POINTS);
     }
 
+    /**
+     * Returns a tag with an integer name
+     * @param sql the string sql query to fetch the tag
+     * @param type the type of tag it is
+     * @return an integer
+     */
     private int getIntTag(String sql, TagType type)
     {
         try (Connection conn = databaseManager.connect();
