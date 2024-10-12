854d290118323b714436c9349b316a380cd88c51
Caleb <cco139@uclive.ac.nz>
2024-10-12 17:49:42 +1300

    Removed system.out.println()'s

--- src/main/java/seng202/team1/gui/controllers/SearchWineController.java
+++ src/main/java/seng202/team1/gui/controllers/SearchWineController.java
@@ -552,7 +552,6 @@ public class SearchWineController {
     @FXML
     public void displayCurrentPage()
     {
-        System.out.println("IM BEING CALLED");
         allWines = SearchWineService.getInstance().getWineList();
 
         if (allWines == null) {
@@ -617,7 +616,6 @@ public class SearchWineController {
             int currentCol = i % columns;
 
             try {
-                System.out.println();
                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/wineMiniDisplay.fxml"));
                 Parent parent = fxmlLoader.load();
                 wineGrid.add(parent, currentCol, currentRow);
@@ -677,7 +675,6 @@ public class SearchWineController {
     public void pageEnd()
     {
         currentPage = Math.ceilDiv(allWines.size(), MAXSIZE) - 1;
-        System.out.println(currentPage);
         displayCurrentPage();
     }
 
